06ab7a847a6f739dcce8d86a232b22f076cb857e
Tio Haidar <atiohaidar@gmail.com>
2024-06-18 03:32:44 +0700

    jadi web dulu

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const { Telegraf } = require('telegraf')
-const { message } = require('telegraf/filters')
+const express = require('express')
+const app = express()
+const port = 3000
 
-const bot = new Telegraf("7113259897:AAEssuqa1NWfHrRqubO2S0b00O8Pl_3k7AA")
-console.log("Mulai")
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
