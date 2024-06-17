581f4a032c3b0d58d4577ea10446796cc0c5cfc8
Tio Haidar <atiohaidar@gmail.com>
2024-06-18 03:01:12 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+
+const bot = new Telegraf("7113259897:AAEssuqa1NWfHrRqubO2S0b00O8Pl_3k7AA")
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
