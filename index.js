6b32bb6249ca5c3a6d4363355a99cf0ad7782237
Tio Haidar <atiohaidar@gmail.com>
2024-06-18 11:09:53 +0700

    c

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!. Naamaku Tio')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+console.log("Halllo")
